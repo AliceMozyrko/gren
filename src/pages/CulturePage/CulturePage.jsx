aa41a5c49fdf40f023647bc2e6636cb5351d6de8
AliceMozyrko <mozyrkoalisa@gmail.com>
2025-04-20 16:02:47 +0300

    frog developer added

--- src/pages/CulturePage/CulturePage.jsx
+++ src/pages/CulturePage/CulturePage.jsx
@@ -1,3 +1,11 @@
+import frogDeveloper from "../../assets/images/develop.gif"
+import css from "./CulturePage.module.css"
+
 export default function CulturePage() {
-  
+  return (
+    <div>
+      <img src={frogDeveloper} alt="frogdeveloper" className={css.frogIcon} />
+      <p className={css.txt}>Oups... GREN est encore en développement !</p>
+    </div>
+  )
 }
--- src/pages/JouerPage/JouerPage.jsx
+++ src/pages/JouerPage/JouerPage.jsx
@@ -1,3 +1,11 @@
+import frogDeveloper from "../../assets/images/develop.gif"
+import css from "./JouerPage.module.css"
+
 export default function JouerPage() {
-  
+ return (
+          <div>
+            <img src={frogDeveloper} alt="frogdeveloper" className={css.frogIcon} />
+            <p className={css.txt}>Oups... GREN est encore en développement !</p>
+          </div>
+        )
 }
